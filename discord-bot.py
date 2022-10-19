320482c0760f48eacbc9b3e4e3704b14360dc85b
Flippchen <91947480+flippchen@users.noreply.github.com>
2022-10-19 17:32:50 +0200

    bot status ok

--- discord-bot.py
+++ discord-bot.py
@@ -1,11 +1,11 @@
 import discord
 from discord import app_commands
 import config
+
 intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
-
 
 
 @client.event
@@ -13,12 +13,14 @@ async def on_ready():
     await sync_commands()
 
 
-@tree.command(name="todaysplan", description="Displays today's meal plan", guild=discord.Object(id=config.discord_object_id))
+@tree.command(name="todaysplan", description="Displays today's meal plan",
+              guild=discord.Object(id=config.discord_object_id))
 async def displays_todays_meal_plan(ctx):
     await ctx.response.send_message(f"Das Essen wurde heute nicht gekocht")
 
 
-@tree.command(name="weeklyplan", description="Displays the weekly meal plan", guild=discord.Object(id=config.discord_object_id))
+@tree.command(name="weeklyplan", description="Displays the weekly meal plan",
+              guild=discord.Object(id=config.discord_object_id))
 async def displays_weekly_meal_plan(ctx):
     await ctx.response.send_message(f"Das Essen wurde diese Woche nicht gekocht")
 
