56fcbaa82cf8b7d96dae6d7838f65a36d941e727
mfloto <60036186+mfloto@users.noreply.github.com>
2022-10-19 20:37:30 +0200

    added daily and weekly menu

--- canteen_plan/canteen_plan.py
+++ canteen_plan/canteen_plan.py
@@ -1,23 +1,51 @@
-import pandas
+import pandas as pd
 import requests
 import datetime
 import config
+from calendar import day_name
 
 
-def get_daily_menu() -> str:
+def get_daily_menu(offset: int) -> str:
     current_day = datetime.datetime.today().weekday()
+    current_day += offset
+
+    # Weekend -> Monday
     if current_day == 5 or current_day == 6:
         current_day = 0
 
-    url = config.mensa_endpoint.replace("CURRENT_DATE", datetime.datetime.today().strftime("%Y-%m-%d"))
-
-    # https://www.stw-ma.de/Essen+_+Trinken/Speisepl%C3%A4ne/Mensaria+Metropol-date-2022%25252d10%25252d19-view-week.html
-
+    time_delta = datetime.timedelta(days=offset) + datetime.datetime.today()
+    url = config.mensa_endpoint.replace("CURRENT_DATE",
+                                        f"{time_delta.strftime('%Y')}%25252d{time_delta.strftime('%m')}%25252d{time_delta.strftime('%d')}")
     res = requests.get(url)
-    df_plan = pandas.read_html(res.text)[0]
-    if df_plan.loc[current_day * 2].count() == 1:
+    df_plan = pd.read_html(res.text)[0]
+
+    day_idx = (current_day % 7) * 2
+
+    # check if day has data
+    if df_plan.loc[day_idx].count() == 1:
         return "Die Mensa hat geschlossen!"
-    return df_plan.loc[current_day * 2].to_string()
+
+    pd.set_option('display.max_colwidth', None)
+    df_day = df_plan.loc[day_idx]
+
+    menu_string = ""
+
+    for i in range(1, len(df_day)):
+        menu_string += f"```{df_day.index[i]}: {df_day[i]}```"
+            
+    return menu_string
 
 
-print(get_daily_menu())
+async def get_weekly_menu(ctx, weekly_offset: int) -> None:
+    next_week = datetime.timedelta(weeks=weekly_offset) + datetime.datetime.today()
+    next_monday = next_week - datetime.timedelta(days=next_week.weekday())
+    offset = (next_monday - datetime.datetime.today()).days + 1
+
+    await ctx.response.send_message(f"Der Essensplan für KW {next_week.isocalendar().week}:")
+    
+    for i in range(offset, offset + 5):
+        print(i)
+        print(get_daily_menu(i)[:50])
+        await ctx.channel.send(f"**{day_name[i % offset]} ({(datetime.datetime.today() + datetime.timedelta(days=i)).strftime('%d.%m.%Y')}):**" + get_daily_menu(i))
+
+    
--- discord-bot.py
+++ discord-bot.py
@@ -2,6 +2,8 @@ import discord
 from discord import app_commands
 import config
 
+from canteen_plan.canteen_plan import get_daily_menu, get_weekly_menu
+
 intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
@@ -15,14 +17,17 @@ async def on_ready():
 
 @tree.command(name="todaysplan", description="Displays today's meal plan",
               guild=discord.Object(id=config.discord_object_id))
-async def displays_todays_meal_plan(ctx):
-    await ctx.response.send_message(f"Das Essen wurde heute nicht gekocht")
+async def displays_todays_meal_plan(ctx, tomorrow: bool = False):
+    if tomorrow:
+        await ctx.response.send_message(get_daily_menu(1))
+    else:
+        await ctx.response.send_message(get_daily_menu(0))
 
 
 @tree.command(name="weeklyplan", description="Displays the weekly meal plan",
               guild=discord.Object(id=config.discord_object_id))
-async def displays_weekly_meal_plan(ctx):
-    await ctx.response.send_message(f"Das Essen wurde diese Woche nicht gekocht")
+async def displays_weekly_meal_plan(ctx, weekly_offset: int = 0):
+    await get_weekly_menu(ctx, weekly_offset)
 
 
 async def sync_commands():
